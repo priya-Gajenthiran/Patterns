7c79a5892ae9635daaf1d30b850c38d9f6e4e959
priya-Gajenthiran <priyagajendran05@gmail.com>
2025-08-09 12:59:55 +0530

    Create Star Box Frame.java

---
+++ Star Box Frame.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i==0|| j==0 ||i==n-1 ||j==n-1){
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
