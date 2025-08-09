817b205cc1402251a11f7f49100efe0161c24ab6
priya-Gajenthiran <priyagajendran05@gmail.com>
2025-08-09 13:01:14 +0530

    Create Diamond Star Pattern.java

---
+++ Diamond Star Pattern.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++){
+            int space=n-i-1;
+            for(int j=0;j<space;j++){
+                System.out.print(" ");
+            }
+            int star=2*i+1;
+            for(int k=0;k<star;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=n-1;i>0;i--){
+            int sp=n-i-1;
+            for(int j=0;j<=sp;j++){
+                System.out.print(" ");
+            }
+            int star=2*i-1;
+            for(int k=0;k<star;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
+    }
+}
